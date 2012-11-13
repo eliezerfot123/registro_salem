94c8676dc4470e34b931edf7773f42c885faba1b
Eliezer Romero <eliezer@(none)>
2012-11-13 14:27:43 -0430

    Añadiendo especificaciones al admin.py de inscriciones

--- inscripciones/admin.py
+++ inscripciones/admin.py
@@ -6,6 +6,7 @@ from lib import admin as Autocompletar
 class InscripcionesAdmin(Autocompletar.Autocompletar):
     form=Autocompletar.make_ajax_form(Inscripcion,dict(persona='personas'))
     search_fields=['persona__cedula', 'persona__nombre', 'persona__apellido']
+    list_display=('persona','periodo','anfitrion','iglesia','observacion')
     list_filter=['periodo']
 admin.site.register(Inscripcion, InscripcionesAdmin)
 
